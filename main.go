22abb1510b6877c24fad6faab9b4e76c8810fecb
Oleksandr Vorobiov <alcyxob@gmail.com>
2024-11-27 22:40:53 +0200

    fix: added mongo code

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -27,30 +28,41 @@ var excercises = []excercise{
 	{ID: "3", Title: "Pull ups", Muscles: "Back", Type: "Pull"},
 }
 
+// A global variable that will hold a reference to the MongoDB client
+var mongoClient *mongo.Client
+
+// The init function will run before our main function to establish a connection to MongoDB. If it cannot connect it will fail and the program will exit.
+func init() {
+	if err := connect_to_mongodb(); err != nil {
+		log.Fatal("Could not connect to MongoDB")
+	}
+}
+
+// Our implementation logic for connecting to MongoDB
+func connect_to_mongodb() error {
+	uri := os.Getenv("MONGODB_URI")
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	mongoClient = client
+	return err
+}
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
-	uri := os.Getenv("MONGODB_URI")
-	docs := "www.mongodb.com/docs/drivers/go/current/"
-	if uri == "" {
-		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
-			"See: " + docs +
-			"usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().
-		ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
 
 	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
 	router.GET("/excercises", getExcercises)
 	router.GET("/excercises/:id", getExcersiseByID)
 	router.POST("/excercises", postExcercise)
@@ -58,9 +70,22 @@ func main() {
 	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
+// getExcercises responds with the list of all albums as JSON.
 func getExcercises(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, excercises)
+	// Find movies
+	cursor, err := mongoClient.Database("fitnnes").Collection("excercises").Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	// Map results
+	var excercises []bson.M
+	if err = cursor.All(context.TODO(), &excercises); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	// Return movies
+	c.JSON(http.StatusOK, excercises)
 }
 
 // postExcercises adds an excersise from JSON received in the request body.
@@ -68,28 +93,31 @@ func postExcercise(c *gin.Context) {
 	var newExcercise excercise
 
 	// Call BindJSON to bind the received JSON to
-	// newAlbum.
+	// newExcercise.
 	if err := c.BindJSON(&newExcercise); err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	excercises = append(excercises, newExcercise)
-	c.IndentedJSON(http.StatusCreated, newExcercise)
+	coll := mongoClient.Database("fitnnes").Collection("excercises")
+	result, err := coll.InsertOne(context.TODO(), newExcercise)
+	if err != nil {
+		panic(err)
+	}
+	c.IndentedJSON(http.StatusCreated, result)
 }
 
 // getExcersiseByID locates the excercise whose ID value matches the id
 // parameter sent by the client, then returns that excercise as a response.
 func getExcersiseByID(c *gin.Context) {
+	// Get excercise ID from URL
 	id := c.Param("id")
-
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range excercises {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	// Find excercise by ID
+	var excercise bson.M
+	err := mongoClient.Database("fitnnes").Collection("excercises").FindOne(context.TODO(), bson.D{{"_id", id}}).Decode(&excercise)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "excercise not found"})
+	// Return excercise
+	c.JSON(http.StatusOK, excercise)
 }
